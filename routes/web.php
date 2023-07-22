e9a292c09e0a3d275db0413b31fd8e03bcd0dd9c
EC2 Default User <ec2-user@ip-172-31-27-246.ap-northeast-1.compute.internal>
2023-07-22 15:54:12 +0000

    implemented function to add books

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,42 @@
 
 
 Route::get('/', function () {
-    return view('books');
+    $books = Book::orderBy('created_at','asc')->get();
+    return view('books',[
+        'books'=>$books    
+    ]);
+    //return view('books');
 });
 
 /**
  * 本を追加
  */
-Route::post('/books',function(Request $request){
-    //
+Route::post('/books', function (Request $request) {
+
+    
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255',
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+      
+    // Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save(); 
+    return redirect('/');
+
+
 });
 
 /**
